621f1a66049170bbda2140a6173b237e07e573f4
Baptiste Bessard <bessardbusiness@gmail.com>
2024-04-16 15:17:26 +0200

    image.c

--- src/main.c
+++ src/main.c
@@ -24,35 +24,14 @@ int	main(int ac, char **av)
 
 	// TO DO : parser les args
 
-	// TO DO : Initialisation de la fenêtre MLX
-	// mlx_init create a xvar struct and return a pointer to it;
-	data.mlx_ptr = mlx_init();
-	if (data.mlx_ptr == NULL)
-		return 1;
-
-	// TO DO : Create a new window ; read from an image (PNG format leaks memory)
-	data.win_ptr = mlx_new_window(data.mlx_ptr, (MAP_WIDTH + 1) * TILE_SIZE, (MAP_HEIGHT + 1) * TILE_SIZE, "Cub3D");
-    if (data.win_ptr == NULL)
-	{
-		// TO DO : destroy window ? display ? close ? mlx_destroy_display(mlx_ptr);
-		return 1;
-	}
+	// Initialisation de la fenêtre MLX 
+	my_mlx_init(&data);
 	
 	// TO DO : Initialize game data : the map, player, textures, etc.
     // init_game(&data);
 	// init_map(&data);
 	// init_player(&data);
 	// init_textures(&data);
-	
-	// TO DO : delete because already one image ? 
-	// img.img = mlx_xpm_file_to_image(mlx_ptr, relative_path, &img.win_width, &img.win_height);
-	// img.img = mlx_new_image(mlx_ptr, MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE);
-
-	// ** After creating an image, we can call `mlx_get_data_addr`, we pass
-	// ** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-	// ** then be set accordingly for the *current* data address.
-	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-	// 							&img.endian);
 
 	// TO DO : LISTEN FOR INPUTS
 
@@ -60,7 +39,7 @@ int	main(int ac, char **av)
 	// TO DO : create function render_next_frame function to render images
 	// int	render_next_frame(void *YourStruct);
 	// mlx_loop_hook(data.mlx_ptr, render_next_frame, &data);
-	
+
 	// Main loop
 	mlx_loop(data.mlx_ptr);
 
--- src/rendering/image.c
+++ src/rendering/image.c
@@ -1,1 +1,40 @@
 #include "cub3d-bis.h"
+
+// mlx_init create a xvar struct and return a pointer to it;
+
+int	my_mlx_init(t_data *data)
+{
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return 1;
+
+	// TO DO : Create a new window ; read from an image (PNG format leaks memory)
+	data.win_ptr = mlx_new_window(data.mlx_ptr, (MAP_WIDTH + 1) * TILE_SIZE, (MAP_HEIGHT + 1) * TILE_SIZE, "Cub3D");
+    if (data.win_ptr == NULL)
+	{
+		// TO DO : destroy window ? display ? close ? mlx_destroy_display(mlx_ptr);
+		return 1;
+	}
+
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
+
+	// TO DO : delete because already one image ? 
+	// img.img = mlx_xpm_file_to_image(mlx_ptr, relative_path, &img.win_width, &img.win_height);
+	// img.img = mlx_new_image(mlx_ptr, MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE);
+
+// GET THE IMAGE ADRESS
+	// ** After creating an image, we can call `mlx_get_data_addr`, we pass
+	// ** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
+	// ** then be set accordingly for the *current* data address.
+	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	// 							&img.endian);
