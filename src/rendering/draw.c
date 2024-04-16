df165cad467743960ea63ef9182eaf9bb15a628a
Baptiste Bessard <bessardbusiness@gmail.com>
2024-04-16 15:57:23 +0200

    image.c mlx init using t_data structure

--- src/rendering/draw.c
+++ src/rendering/draw.c
@@ -1,13 +1,1 @@
 #include "cub3d-bis.h"
-
-// L'offset mémoire pour un pixel à une certaine position (x, y) dans l'image
-// y * line_length : cela détermine la position verticale dans l'image en prenant en compte la longueur de la ligne.
-// x * (bits_per_pixel / 8) : cela détermine la position horizontale en prenant en compte le nombre de bits par pixel et le divisant par 8 pour obtenir le nombre d'octets.
-// int offset = (y * line_length + x * (bits_per_pixel / 8));
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->image.addr + (y * data->image.line_length + x * (data->image.bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
--- src/rendering/image.c
+++ src/rendering/image.c
@@ -1,31 +1,47 @@
 #include "cub3d-bis.h"
 
-// mlx_init create a xvar struct and return a pointer to it;
+// L'offset mémoire pour un pixel à une certaine position (x, y) dans l'image
+// y * line_length : cela détermine la position verticale dans l'image en prenant en compte la longueur de la ligne.
+// x * (bits_per_pixel / 8) : cela détermine la position horizontale en prenant en compte le nombre de bits par pixel et le divisant par 8 pour obtenir le nombre d'octets.
+// int offset = (y * line_length + x * (bits_per_pixel / 8));
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
 
+	dst = data->image.addr + (y * data->image.line_length + x * (data->image.bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+// mlx_init create a xvar struct and return a pointer to it;
 int	my_mlx_init(t_data *data)
 {
 
-	data.mlx_ptr = mlx_init();
-	if (data.mlx_ptr == NULL)
+	data->mlx_ptr = mlx_init();
+	if (data->mlx_ptr == NULL)
 		return 1;
 
 	// TO DO : Create a new window ; read from an image (PNG format leaks memory)
-	data.win_ptr = mlx_new_window(data.mlx_ptr, (MAP_WIDTH + 1) * TILE_SIZE, (MAP_HEIGHT + 1) * TILE_SIZE, "Cub3D");
-    if (data.win_ptr == NULL)
+	data->win_ptr = mlx_new_window(data->mlx_ptr, (MAP_WIDTH + 1) * TILE_SIZE, (MAP_HEIGHT + 1) * TILE_SIZE, "Cub3D");
+    if (data->win_ptr == NULL)
 	{
-		// TO DO : destroy window ? display ? close ? mlx_destroy_display(mlx_ptr);
+		mlx_destroy_display(data->mlx_ptr); // Clean up
 		return 1;
 	}
 
+	 // Get image address
+    data->image.addr = mlx_get_data_addr(data->image.img, &(data->image.bits_per_pixel), &(data->image.line_length),
+                                         &(data->image.endian));
+    
+    // Example usage of my_mlx_pixel_put
+    my_mlx_pixel_put(&data->image, 5, 5, 0x00FF0000);
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+    // Put image to window
+    mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->image.img, 0, 0);
+
+    // Enter the event loop
+    mlx_loop(data->mlx_ptr);
+
+    return 0;
 }
 
 	// TO DO : delete because already one image ? 
