686c65b1bb722af2aa5ee92dd93358eb27c9caad
Baptiste Bessard <bessardbusiness@gmail.com>
2024-05-05 19:56:07 +0200

    player movement not ready

--- srcs/moving/listener.c
+++ srcs/moving/listener.c
@@ -19,7 +19,15 @@ void	loop(t_data *data)
 	mlx_hook(data->mlx.mlx_win_ptr, KEY_PRESS, 1L << 0, key_press, data);
 	mlx_hook(data->mlx.mlx_win_ptr, KEY_RELEASE, 1L << 1, key_release, data);
 	mlx_hook(data->mlx.mlx_win_ptr, 17, 1L << 17, exit_game, data);
+	mlx_loop_hook(data->mlx.mlx_ptr, move_player, data);
 	mlx_loop_hook(data->mlx.mlx_ptr, render_next_frame, data);
+	printf("Key W: %d\n", data->keys->w);
+    printf("Key A: %d\n", data->keys->a);
+    printf("Key S: %d\n", data->keys->s);
+    printf("Key D: %d\n", data->keys->d);
+    printf("Key ESC: %d\n", data->keys->esc);
+    printf("Key LEFT: %d\n", data->keys->left);
+    printf("Key RIGHT: %d\n", data->keys->right);
 	mlx_loop(data->mlx.mlx_ptr);
 }
 
