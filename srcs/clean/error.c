809b6d2c53812eeaf1c5a612e26a9701daec2d9c
Baptiste Bessard <bessardbusiness@gmail.com>
2024-05-22 16:44:15 +0200

    norm42

--- srcs/clean/error.c
+++ srcs/clean/error.c
@@ -19,8 +19,13 @@ void debug_ray(t_ray *ray) {
         return;
     }
     // printf("ray->side = %d\n", ray->side); // TO DO CORRECT VALUE HERE
-    printf("ray->x = %d\n", ray->x);
-    printf("ray->y = %d\n", ray->y);
+    printf("ray->x1 = %d\n", ray->x1);
+    printf("ray->y1 = %d\n", ray->y1);
+	printf("ray->x2 = %d\n", ray->x2);
+    printf("ray->y2 = %d\n", ray->y2);
+	printf("ray->step = %d\n", ray->steps);
+	printf("ray->step_x = %d\n", ray->step_x);
+	printf("ray->step_y = %d\n", ray->step_y);
     printf("ray->dir_x = %f\n", ray->dir_x);
     printf("ray->dir_y = %f\n", ray->dir_y);
     printf("ray->map_x = %f\n", ray->map_x);
